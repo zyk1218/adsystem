6a159285344cd96bceb19fada72bfa8026d2eeae
remember <1977293486@qq.com>
2020-04-16 10:33:31 +0800

    deploy the signal eureka-server

---
+++ ad-eureka/src/main/java/com/remember/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.remember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+  * @author remember
+  * @date 2020/4/16 10:12
+ * 启动开关
+  */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
