8659321564a1f548f04e47216ec26009de597a6b
remember <1977293486@qq.com>
2020-04-21 10:40:40 +0800

    广告检索模块框架搭建

---
+++ ad-service/ad-search/src/main/java/com/remember/SearchApplication.java
@@ -0,0 +1,27 @@
+package com.remember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+  * @author remember
+  * @date 2020/4/21 10:26
+  */
+@EnableFeignClients //使用feign去访问其他微服务
+@EnableEurekaClient //作为Eureka
+@EnableHystrix //断路器
+@EnableCircuitBreaker //断路器
+@EnableDiscoveryClient //微服务的发现能力
+@EnableHystrixDashboard //Hystrix的监控
+@SpringBootApplication
+public class SearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class,args);
+    }
+}
