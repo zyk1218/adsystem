f000467e4abf1b372bcd5d67f22773064f035bb1
remember <1977293486@qq.com>
2020-04-23 19:51:54 +0800

    完成创意索引对象的定义与服务实现

---
+++ ad-service/ad-search/src/main/java/com/remember/index/creative/CreativeIndex.java
@@ -0,0 +1,56 @@
+package com.remember.index.creative;
+
+import com.remember.index.IndexAware;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+  * @author remember
+  * @date 2020/4/23 19:32
+  */
+@Component
+@Slf4j
+public class CreativeIndex implements IndexAware<Long,CreativeObject> {
+
+    private static Map<Long,CreativeObject> objectMap;
+
+    static {
+        objectMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public CreativeObject get(Long key) {
+        return objectMap.get(key);
+    }
+
+    @Override
+    public void update(Long key, CreativeObject value) {
+        log.info("objectMap before update : {}",objectMap);
+        CreativeObject old = objectMap.get(key);
+        if(old == null){
+            objectMap.put(key,value);
+        }else{
+            old.update(value);
+        }
+        log.info("objectMap after update : {}",objectMap);
+
+    }
+
+    @Override
+    public void delete(Long key, CreativeObject value) {
+        log.info("objectMap before delete : {}",objectMap);
+        objectMap.remove(key);
+        log.info("objectMap after delete : {}",objectMap);
+
+    }
+
+    @Override
+    public void add(Long key, CreativeObject value) {
+        log.info("objectMap before add : {}",objectMap);
+        objectMap.put(key,value);
+        log.info("objectMap after add : {}",objectMap);
+    }
+}
