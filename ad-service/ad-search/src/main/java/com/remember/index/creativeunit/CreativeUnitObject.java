f334f75a8e01c5961009198f0d5e7a552c47e446
remember <1977293486@qq.com>
2020-04-23 20:17:13 +0800

    完成了创意与推广单元联系实体类的定义与服务实现

---
+++ ad-service/ad-search/src/main/java/com/remember/index/creativeunit/CreativeUnitObject.java
@@ -0,0 +1,18 @@
+package com.remember.index.creativeunit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+  * @author remember
+  * @date 2020/4/23 19:52
+  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitObject {
+    private Long adId;
+    private Long unitId;
+
+}
